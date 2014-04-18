f2c93352af6873d645c99262f70909b08c763676
Andreas Voegele <andreas@andreasvoegele.com>
2014-04-18 17:19:14 +0200

    My implementation of Tennis::Game1

--- Tennis/perl/Tennis.pm
+++ Tennis/perl/Tennis.pm
@@ -3,75 +3,46 @@ use warnings;
 
 package Tennis::Game1;
 
-sub new {
-    my ( $cls, $player1Name, $player2Name ) = @_;
-    my $self = {
-        player1Name => $player1Name,
-        player2Name => $player2Name,
-        p1points    => 0,
-        p2points    => 0,
-    };
-    return bless $self, $cls;
-}
+use Moose;
+use List::AllUtils qw(all any firstidx max);
+use Locale::Maketext::Simple;
+use Tie::IxHash;
+
+# Maps player names to points.  The hash is sorted.
+has points_for => ( is => 'ro', isa => 'HashRef[Int]' );
+
+around BUILDARGS => sub {
+    my ( $orig, $class, @players ) = @_;
+    tie my %points_for, 'Tie::IxHash', map { $_ => 0 } @players;
+    return $class->$orig( points_for => \%points_for );
+};
 
 sub won_point {
-    my ( $self, $playerName ) = @_;
-    if ( $playerName eq $self->{player1Name} ) {
-        $self->{p1points}++;
-    }
-    else {
-        $self->{p2points}++;
-    }
+    my ( $self, $player ) = @_;
+    ++$self->points_for->{$player};
+    return;
 }
 
 sub score {
-    my $self       = shift;
-    my $result     = "";
-    my $tempScore = 0;
+    my $self = shift;
 
-    if ( $self->{p1points} == $self->{p2points} ) {
-        $result = {
-            0 => "Love-All",
-            1 => "Fifteen-All",
-            2 => "Thirty-All",
-          }->{ $self->{p1points} }
-          || "Deuce";
+    my @players = keys %{ $self->points_for };
+    my @points  = values %{ $self->points_for };
+    my $max = max @points;
+    if ( all { $_ == $max } @points ) {
+        my @tie_scores = qw(Love-All Fifteen-All Thirty-All);
+        return loc( $tie_scores[ $points[0] ] // 'Deuce' );
     }
-
-    elsif ($self->{p1points}>=4 or $self->{p2points}>=4) {
-        my $minusResult = $self->{p1points}-$self->{p2points};
-        if ($minusResult==1) {
-            $result ="Advantage " . $self->{player1Name};
-        } elsif ($minusResult ==-1) {
-            $result ="Advantage " . $self->{player2Name};
-        } elsif ($minusResult>=2) {
-            $result = "Win for " . $self->{player1Name};
-        } else {
-            $result ="Win for " . $self->{player2Name};
+    if ( $max >= 4 ) {
+        my $leading_player = $players[ firstidx { $_ == $max } @points ];
+        if ( any { $max - $_ == 1 } @points ) {
+            return loc( 'Advantage [_1]', $leading_player );
         }
+        return loc( 'Win for [_1]', $leading_player );
     }
-
-    else {
-        foreach  my $i (1 .. 2) {
-            if ($i==1) {
-                $tempScore = $self->{p1points};
-            } else {
-                $result.="-";
-                $tempScore = $self->{p2points};
-            }
-
-            $result .= {
-                0 => "Love",
-                1 => "Fifteen",
-                2 => "Thirty",
-                3 => "Forty",
-            }->{$tempScore};
-        }
-    }
-
-    return $result;
+    my @scores = qw(Love Fifteen Thirty Forty);
+    return join '-', map { loc( $scores[$_] ) } @points;
 }
-
 
 package Tennis::Game2;
 
