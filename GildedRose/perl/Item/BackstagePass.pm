e8f84de7e6eea8450732bb523d696cbc6b059262
Andreas Voegele <andreas@andreasvoegele.com>
2014-04-19 15:05:30 +0200

    Add "periods_of_high_demand"

--- GildedRose/perl/Item/BackstagePass.pm
+++ GildedRose/perl/Item/BackstagePass.pm
@@ -3,24 +3,32 @@ package Item::BackstagePass;
 use Moose;
 extends 'ItemDelegator';
 
+has periods_of_high_demand => (
+    is      => 'ro',
+    isa     => 'ArrayRef[Int]',
+    builder => '_build_periods_of_high_demand',
+);
+
+sub _build_periods_of_high_demand {
+    my $self = shift;
+    return [ 10, 5 ];
+}
+
 sub update_quality {
     my $self = shift;
     if ( $self->quality < $self->maximum_quality ) {
         $self->inc_quality;
-        if ( $self->sell_in < 11 ) {
-            if ( $self->quality < $self->maximum_quality ) {
-                $self->inc_quality;
-            }
-        }
-        if ( $self->sell_in < 6 ) {
-            if ( $self->quality < $self->maximum_quality ) {
-                $self->inc_quality;
+        for my $period ( @{ $self->periods_of_high_demand } ) {
+            if ( $self->sell_in <= $period ) {
+                if ( $self->quality < $self->maximum_quality ) {
+                    $self->inc_quality;
+                }
             }
         }
     }
     $self->dec_sell_in;
     if ( $self->sell_in < 0 ) {
-        $self->set_quality($self->minimum_quality);
+        $self->set_quality( $self->minimum_quality );
     }
     return;
 }
